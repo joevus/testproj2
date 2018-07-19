2d99facc8845ac82c8a1c9e3c916ec336151eb8c
joevus <joehoskisson@gmail.com>
2018-07-19 15:27:08 -0600

    filled in demo controller.

--- app/controllers/post.js
+++ app/controllers/post.js
@@ -1,4 +1,29 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
+  actions: {
+    createPost() {
+      let newPost = this.get('newPost');
+      let newRecord = this.store.createRecord('post', {
+        title: newPost
+      });
+      newPost.save();
+    },
+    readPost() {
+      this.store.findRecord('post', 1).then((post) => {
+        alert(post.get('title') + ' ' + game.get('id'))
+      });
+    },
+    updatePost() {
+      let updatedTitle = this.get('updatedTitle');
+      let post = this.get('model').findBy('id', '1');
+      post.set('title', updatedTitle);
+      post.save();
+    },
+    destroyPost() {
+      let destroyId = this.get('destroyId');
+      let post = this.get('model').findBy('id', destroyId);
+      post.destroyRecord;
+    }
+  }
 });
