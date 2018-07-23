d7c1965848499024e18eb826f095d053ca6b1c31
joevus <joehoskisson@gmail.com>
2018-07-23 09:36:09 -0600

    Can upload attachment now.

--- app/controllers/posts.js
+++ app/controllers/posts.js
@@ -5,12 +5,52 @@ export default Controller.extend({
     createPost() {
       let newTitle = this.get('newTitle');
       let newContent = this.get('newContent') || 'default content';
-      let newRecord = this.store.createRecord('post', {
-        title: newTitle,
-        content: newContent,
-        key: "joe.hoskisson"
-      });
-      newRecord.save();
+      let newFile = document.querySelector("#new-file").files[0];
+      const reader = new FileReader();
+      let fileData;
+      reader.onload = () => {
+        fileData = reader.result;
+        let newRecord = this.store.createRecord('post', {
+          title: newTitle,
+          content: newContent,
+          attachment: fileData,
+          key: "joe.hoskisson"
+        });
+        newRecord.save();
+      }
+
+      if (newFile) {
+        reader.readAsDataURL(newFile);
+      } else {
+         let newRecord = this.store.createRecord('post', {
+            title: newTitle,
+            content: newContent,
+            attachment: fileData,
+            key: "joe.hoskisson"
+          });
+          newRecord.save();
+      }
+
+      console.log(fileData);
+      // reader.onload = () => {
+      //   fileData = reader.result;
+      //   newRecord = this.store.createRecord('post', {
+      //     title: newTitle,
+      //     content: newContent,
+      //     attachment: fileData,
+      //     key: "joe.hoskisson"
+      //   });
+      //   newRecord.save();
+      //   console.log(fileData);
+      //   console.log(newRecord);
+      // }
+      // let newRecord = this.store.createRecord('post', {
+      //   title: newTitle,
+      //   content: newContent,
+      //   attachment: fileData,
+      //   key: "joe.hoskisson"
+      // });
+      // newRecord.save();
     },
     readPost() {
       this.store.findRecord('post', 1).then((post) => {
