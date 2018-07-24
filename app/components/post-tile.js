d66fa42c38d8538eec9e1eeede37265091c6f2e7
joevus <joehoskisson@gmail.com>
2018-07-24 13:58:42 -0600

    Got filedata to be passed to post controller.

--- app/components/post-tile.js
+++ app/components/post-tile.js
@@ -2,6 +2,10 @@ import Component from '@ember/component';
 
 export default Component.extend({
   actions: {
+    handleFileLoad(event) {
+      let file = event.target.files[0];
+      this.get('onFileLoad')(file);
+    },
     handleUpdate(postId) {
       let updatedTitle = this.get('updatedTitle');
       let updatedContent = this.get('updatedContent');
--- app/controllers/post.js
+++ app/controllers/post.js
@@ -2,6 +2,33 @@ import Controller from '@ember/controller';
 
 export default Controller.extend({
   actions: {
+    fileData: null,
+    fileLoad(file) {
+      console.log("triggered fileLoad:" );
+      console.log(file);
+      const reader = new FileReader();
+      let fileData;
+      // maybe see if can put this in database later
+      // let filePath;
+      reader.onload = () => {
+        fileData = reader.result;
+        this.fileData = fileData;
+        console.log(this.fileData);
+      }
+
+      if (file) {
+        reader.readAsDataURL(file);
+      } else {
+         let newRecord = this.store.createRecord('post', {
+            title: newTitle,
+            content: newContent,
+            attachment: fileData,
+            key: "joe.hoskisson"
+          });
+          newRecord.save();
+      }
+
+    },
     updatePost(postId, updatedTitle, updatedContent) {
       if(updatedContent === undefined) {
         updatedContent = '';
